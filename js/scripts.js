d958e7b03cb9b3584cc890a1c43ca054b3f3afe3
Jahongir-veb <jahongirmh@gmail.com>
2021-01-07 17:02:45 +0500

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 var elForm = document.querySelector(`.js-movie-form`);
 var elNameInput = elForm.querySelector(`.movie-name-input`);
 var elYearInput = elForm.querySelector(`.movie-year-input`);
-var elGenreInput = elForm.querySelector(`.movie-genre-input`);
+var elGenreSelect = elForm.querySelector(`.movie-genre-select`);
 var elRatingInput = elForm.querySelector(`.movie-rating-input`);
 var elResultBox = document.querySelector(`.box-right-content`);
 
@@ -19,12 +19,32 @@ var kinolar = movies.map(function (movie){
     year: movie.movie_year,
     imdbId: movie.imdb_id,
     categories: movie.Categories.split(`|`),
-    youtubeId: movie.ytid,
-    imdbId: movie.imdb_id
+    youtubeId: movie.ytid
   }
 });
 
 console.log(kinolar);
+
+var genres = [];
+
+kinolar.forEach(function(kino) {
+  kino.categories.forEach(function(genre){
+    if (!genres.includes(genre)) {
+      genres.push(genre);
+    }
+  });
+});
+
+console.log(genres);
+
+
+genres.forEach(function(janr){
+  var elOption = document.createElement(`option`);
+  elOption.textContent = janr;
+  elOption.setAttribute(`value`, janr);
+  elGenreSelect.appendChild(elOption);
+});
+
 
 
 elForm.addEventListener(`submit`, function(evt) {
@@ -35,13 +55,14 @@ elForm.addEventListener(`submit`, function(evt) {
 
   var kinolarNames = kinolar.filter(function (kino){
 
-    if(elNameInput.value === `` || elNameInput.value === ` `){
+    if(elNameInput.value.trim() === ``){
       return
     } else
 
     {
+      var doesCategoryMatch = elGenreSelect.value === `all` || kino.categories.includes(elGenreSelect.value);
 
-      return kino.title.toString().match(inputValue);
+      return kino.title.toString().match(inputValue) && (kino.imdbRaiting >= Number(elRatingInput.value)) && doesCategoryMatch;
     }
 
   });
@@ -57,6 +78,7 @@ elForm.addEventListener(`submit`, function(evt) {
     elNameItem.querySelector(`.movie-genre`).textContent = kino.categories;
     elNameItem.querySelector(`.movie-year`).textContent = kino.year;
     elNameItem.querySelector(`.movie-rating`).textContent = kino.imdbRaiting;
+    elNameItem.querySelector(`.movie-img`).setAttribute(`src`, `http://i3.ytimg.com/vi/${kino.youtubeId}/hqdefault.jpg`);
 
 
     fragmentName.appendChild(elNameItem);
@@ -65,34 +87,11 @@ elForm.addEventListener(`submit`, function(evt) {
   elResultBox.appendChild(fragmentName);
 
 
-  var raitingMovies = kinolar.filter(function(raiting){
-    if (!Number(elRatingInput.value)){
-      return
-    }
-    else {
-      return raiting.imdbRaiting >= Number(elRatingInput.value) && raiting.imdbRaiting < 1 + Number(elRatingInput.value);
-    }
-  });
 
-  document.querySelector(`.total`).textContent = raitingMovies.length;
-
-  console.log(raitingMovies);
-
-
-  raitingMovies.forEach (function(raiting) {
-    var elNameItem = elTemplate.cloneNode(true);
-
-    elNameItem.querySelector(`.movie-name`).textContent = raiting.title;
-    elNameItem.querySelector(`.movie-genre`).textContent = raiting.categories;
-    elNameItem.querySelector(`.movie-year`).textContent = raiting.year;
-    elNameItem.querySelector(`.movie-rating`).textContent = raiting.imdbRaiting;
-
-
-    fragmentName.appendChild(elNameItem);
-  });
-
-  elResultBox.appendChild(fragmentName);
 });
+
+
+
 
 {/* <template class="movie-template">
 
@@ -107,57 +106,3 @@ elForm.addEventListener(`submit`, function(evt) {
 </div>
 
 </template> */}
-
-
-
-// elGenreForm.addEventListener(`submit`, function(evt) {
-//   evt.preventDefault();
-
-// elGenreList.innerHTML = ``;
-//   var kinolarGenre = kinolar.filter(function (kino){
-//     return kino.genres.includes(elGenreInput.value);
-//   });
-
-//   document.querySelector(`.total-genre`).textContent = kinolarGenre.length;
-//   console.log(kinolarGenre);
-
-//   kinolarGenre.forEach (function(movi) {
-//     var elGenreItem = elTemplate.cloneNode(true);
-
-//     elGenreItem.querySelector(`.movie-name`).textContent = movi.title;
-//     elGenreItem.querySelector(`.movie-genre`).textContent = movi.genres;
-//     elGenreItem.querySelector(`.movie-year`).textContent = movi.year;
-
-//     fragmentGenre.appendChild(elGenreItem);
-//   });
-
-//   elGenreList.appendChild(fragmentGenre);
-
-// });
-
-
-
-// elYearForm.addEventListener(`submit`, function(evt) {
-//   evt.preventDefault();
-
-// elYearList.innerHTML = ``;
-//   var kinolarYear = kinolar.filter(function (kino){
-//     return kino.year === Number(elYearInput.value);
-//   });
-
-//   document.querySelector(`.total-year`).textContent = kinolarYear.length;
-//   console.log(kinolarYear);
-
-//   kinolarYear.forEach (function(movi) {
-//     var elYearItem = elTemplate.cloneNode(true);
-
-//     elYearItem.querySelector(`.movie-name`).textContent = movi.title;
-//     elYearItem.querySelector(`.movie-genre`).textContent = movi.genres;
-//     elYearItem.querySelector(`.movie-year`).textContent = movi.year;
-
-//     fragmentYear.appendChild(elYearItem);
-//   });
-
-//   elYearList.appendChild(fragmentYear);
-
-// });
